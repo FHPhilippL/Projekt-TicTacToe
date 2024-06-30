dfac16525d1f09ff110824726417697275361727
Melina Bachler <melina.bachler@gmail.com>
2024-06-30 10:24:20 +0200

    Add Function to show current GameBoard

--- src/main/java/game/Board.java
+++ src/main/java/game/Board.java
@@ -2,19 +2,20 @@ package game;
 
 public class Board {
     private int size = 3; //Number of Rows and Columns; Default is 3
-    private int[][] field = new int[3][3]; // Playing-field
+    private String[][] field = new String[3][3]; // Playing-field
     private int moveCounter = 0; // Counter for the Number of moves
 
     public Board() { // default constructor
         setSize(3);
         moveCounter = 0;
-        field = new int[3][3];
+        field = new String[3][3];
+        reset();
     }
 
     public Board(int size) { // Constructor with a passed over variable "size"
         setSize(size);
         moveCounter = 0;
-        field = new int[this.size][this.size];
+        field = new String[this.size][this.size];
     }
 
     public int getSize() { // returns the size of the playing-field
@@ -30,12 +31,43 @@ public class Board {
 
     }
 
-    public int[][] getField() { //returns the current field
+    public String[][] getField() { //returns the current field
         return field;
+    }
+
+    /**
+     * For Test Purpose
+     * will be removed after another Methode has been implemented
+     */
+    public void setFieldTest(){
+        field[0][1] = "X";
+        field[0][2] = "O";
+        field[1][0] = "X";
+        field[1][1] = "O";
+        field[1][2] = "X";
+        field[2][0] = "O";
+    }
+
+    public void reset(){ // Resets the Game, to start a new one
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                field[row][col] = " ";
+            }
+        }
     }
 
     public int getMoveCounter() { //returns the current number of moves
         return moveCounter;
     }
 
+
+
+    public String printGameBoard(){ // returns a String of the current GameBoard
+        String s = "";
+        for(int row = 0; row < size; row++){
+            s += (field[row][0] + "|" + field[row][1] + "|" + field[row][2]+"");
+            if(row < 2) s+= "\n-----\n";
+        }
+        return s;
+    }
 }
--- src/test/java/game/BoardTest.java
+++ src/test/java/game/BoardTest.java
@@ -47,4 +47,20 @@ class BoardTest {
         assertEquals(0, board.getMoveCounter());
 
     }
+
+    @Test
+    void assert_that_printGameBoard_works_properly(){
+        //Arrange
+        Board b1 = new Board();
+        Board b2 = new Board();
+        String s1 = " | | \n-----\n | | \n-----\n | | ";
+        String s2 = " |X|O\n-----\nX|O|X\n-----\nO| | ";
+
+        //Act
+        b2.setFieldTest();
+
+        //Assert
+        assertEquals(s1, b1.printGameBoard());
+        assertEquals(s2, b2.printGameBoard());
+    }
 }
